12b1817c10520cd5e1d6c06b2e9da96a90bd7cb8
ArturoExe <amarturin15@gmail.com>
2022-08-10 18:53:12 -0700

    make it better

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000" });
+const API = axios.create({ baseURL: "https://mysharehub.herokuapp.com/" });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("profile")) {
